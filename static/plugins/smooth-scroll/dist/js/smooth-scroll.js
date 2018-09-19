1051be26f25708e18277b870cdc76d057452a1e1
Chris Stayte <ctstayte@gmail.com>
2018-09-18 23:22:37 -0400

    updated bootstrap and smoothscroll

--- static/plugins/smooth-scroll/dist/js/smooth-scroll.js
+++ static/plugins/smooth-scroll/dist/js/smooth-scroll.js
@@ -1,16 +1,16 @@
 /*!
- * smooth-scroll v12.1.5: Animate scrolling to anchor links
- * (c) 2017 Chris Ferdinandi
+ * smooth-scroll v14.2.1: Animate scrolling to anchor links
+ * (c) 2018 Chris Ferdinandi
  * MIT License
  * http://github.com/cferdinandi/smooth-scroll
  */
 
 (function (root, factory) {
-	if ( typeof define === 'function' && define.amd ) {
+	if (typeof define === 'function' && define.amd) {
 		define([], (function () {
 			return factory(root);
 		}));
-	} else if ( typeof exports === 'object' ) {
+	} else if (typeof exports === 'object') {
 		module.exports = factory(root);
 	} else {
 		root.SmoothScroll = factory(root);
@@ -20,17 +20,6 @@
 	'use strict';
 
 	//
-	// Feature Test
-	//
-
-	var supports =
-		'querySelector' in document &&
-		'addEventListener' in window &&
-		'requestAnimationFrame' in window &&
-		'closest' in window.Element.prototype;
-
-
-	//
 	// Default settings
 	//
 
@@ -38,22 +27,40 @@
 		// Selectors
 		ignore: '[data-scroll-ignore]',
 		header: null,
+		topOnEmptyHash: true,
 
 		// Speed & Easing
 		speed: 500,
+		clip: true,
 		offset: 0,
 		easing: 'easeInOutCubic',
 		customEasing: null,
 
-		// Callback API
-		before: function () {},
-		after: function () {}
+		// History
+		updateURL: true,
+		popstate: true,
+
+		// Custom Events
+		emitEvents: true
 	};
 
 
 	//
 	// Utility Methods
 	//
+
+	/**
+	 * Check if browser supports required methods
+	 * @return {Boolean} Returns true if all required methods are supported
+	 */
+	var supports = function () {
+		return (
+			'querySelector' in document &&
+			'addEventListener' in window &&
+			'requestAnimationFrame' in window &&
+			'closest' in window.Element.prototype
+		);
+	};
 
 	/**
 	 * Merge two or more objects. Returns a new object.
@@ -64,9 +71,6 @@
 
 		// Variables
 		var extended = {};
-		var deep = false;
-		var i = 0;
-		var length = arguments.length;
 
 		// Merge the object into the extended object
 		var merge = function (obj) {
@@ -78,13 +82,23 @@
 		};
 
 		// Loop through each object and conduct a merge
-		for ( ; i < length; i++ ) {
-			var obj = arguments[i];
-			merge(obj);
+		for (var i = 0; i < arguments.length; i++) {
+			merge(arguments[i]);
 		}
 
 		return extended;
 
+	};
+
+	/**
+	 * Check to see if user prefers reduced motion
+	 * @param  {Object} settings Script settings
+	 */
+	var reduceMotion = function (settings) {
+		if ('matchMedia' in window && window.matchMedia('(prefers-reduced-motion)').matches) {
+			return true;
+		}
+		return false;
 	};
 
 	/**
@@ -97,10 +111,25 @@
 	};
 
 	/**
+	 * Decode a URI, with error check
+	 * @param  {String} hash The URI to decode
+	 * @return {String}      A decoded URI (or the original string if an error is thrown)
+	 */
+	var decode = function (hash) {
+		var decoded;
+		try {
+			decoded = decodeURIComponent(hash);
+		} catch(e) {
+			decoded = hash;
+		}
+		return decoded;
+	};
+
+	/**
 	 * Escape special characters for use with querySelector
-	 * @param {String} id The anchor ID to escape
 	 * @author Mathias Bynens
 	 * @link https://github.com/mathiasbynens/CSS.escape
+	 * @param {String} id The anchor ID to escape
 	 */
 	var escapeCharacters = function (id) {
 
@@ -171,7 +200,14 @@
 
 		}
 
-		return '#' + result;
+		// Return sanitized hash
+		var hash;
+		try {
+			hash = decodeURIComponent('#' + result);
+		} catch(e) {
+			hash = '#' + result;
+		}
+		return hash;
 
 	};
 
@@ -219,12 +255,14 @@
 
 	/**
 	 * Calculate how far to scroll
-	 * @param {Element} anchor The anchor element to scroll to
-	 * @param {Number} headerHeight Height of a fixed header, if any
-	 * @param {Number} offset Number of pixels by which to offset scroll
+	 * Clip support added by robjtede - https://github.com/cferdinandi/smooth-scroll/issues/405
+	 * @param {Element} anchor       The anchor element to scroll to
+	 * @param {Number}  headerHeight Height of a fixed header, if any
+	 * @param {Number}  offset       Number of pixels by which to offset scroll
+	 * @param {Boolean} clip         If true, adjust scroll distance to prevent abrupt stops near the bottom of the page
 	 * @returns {Number}
 	 */
-	var getEndLocation = function (anchor, headerHeight, offset) {
+	var getEndLocation = function (anchor, headerHeight, offset, clip) {
 		var location = 0;
 		if (anchor.offsetParent) {
 			do {
@@ -233,7 +271,10 @@
 			} while (anchor);
 		}
 		location = Math.max(location - headerHeight - offset, 0);
-		return location;
+		if (clip) {
+			location = Math.min(location, getDocumentHeight() - window.innerHeight);
+		}
+ 		return location;
 	};
 
 	/**
@@ -246,6 +287,32 @@
 	};
 
 	/**
+	 * Update the URL
+	 * @param  {Node}    anchor  The anchor that was scrolled to
+	 * @param  {Boolean} isNum   If true, anchor is a number
+	 * @param  {Object}  options Settings for Smooth Scroll
+	 */
+	var updateURL = function (anchor, isNum, options) {
+
+		// Bail if the anchor is a number
+		if (isNum) return;
+
+		// Verify that pushState is supported and the updateURL option is enabled
+		if (!history.pushState || !options.updateURL) return;
+
+		// Update URL
+		history.pushState(
+			{
+				smoothScroll: JSON.stringify(options),
+				anchor: anchor.id
+			},
+			document.title,
+			anchor === document.documentElement ? '#top' : '#' + anchor.id
+		);
+
+	};
+
+	/**
 	 * Bring the anchored element into focus
 	 * @param {Node}     anchor      The anchor element
 	 * @param {Number}   endLocation The end location to scroll to
@@ -253,12 +320,17 @@
 	 */
 	var adjustFocus = function (anchor, endLocation, isNum) {
 
+		// Is scrolling to top of page, blur
+		if (anchor === 0) {
+			document.body.focus();
+		}
+
 		// Don't run if scrolling to a number on the page
 		if (isNum) return;
 
 		// Otherwise, bring anchor element into focus
 		anchor.focus();
-		if (document.activeElement.id !== anchor.id) {
+		if (document.activeElement !== anchor) {
 			anchor.setAttribute('tabindex', '-1');
 			anchor.focus();
 			anchor.style.outline = 'none';
@@ -268,14 +340,22 @@
 	};
 
 	/**
-	 * Check to see if user prefers reduced motion
-	 * @param  {Object} settings Script settings
+	 * Emit a custom event
+	 * @param  {String} type    The event type
+	 * @param  {Object} options The settings object
+	 * @param  {Node}   anchor  The anchor element
+	 * @param  {Node}   toggle  The toggle element
 	 */
-	var reduceMotion = function (settings) {
-		if ('matchMedia' in window && window.matchMedia('(prefers-reduced-motion)').matches) {
-			return true;
-		}
-		return false;
+	var emitEvent = function (type, options, anchor, toggle) {
+		if (!options.emitEvents || typeof window.CustomEvent !== 'function') return;
+		var event = new CustomEvent(type, {
+			bubbles: true,
+			detail: {
+				anchor: anchor,
+				toggle: toggle
+			}
+		});
+		document.dispatchEvent(event);
 	};
 
 
@@ -300,9 +380,11 @@
 		/**
 		 * Cancel a scroll-in-progress
 		 */
-		smoothScroll.cancelScroll = function () {
-			// clearInterval(animationInterval);
+		smoothScroll.cancelScroll = function (noEvent) {
 			cancelAnimationFrame(animationInterval);
+			animationInterval = null;
+			if (noEvent) return;
+			emitEvent('scrollCancel', settings);
 		};
 
 		/**
@@ -323,13 +405,13 @@
 			var startLocation = window.pageYOffset; // Current location on the page
 			if (animateSettings.header && !fixedHeader) {
 				// Get the fixed header if not already set
-				fixedHeader = document.querySelector( animateSettings.header );
+				fixedHeader = document.querySelector(animateSettings.header);
 			}
 			if (!headerHeight) {
 				// Get the height of a fixed header if one exists and not already set
 				headerHeight = getHeaderHeight(fixedHeader);
 			}
-			var endLocation = isNum ? anchor : getEndLocation(anchorElem, headerHeight, parseInt((typeof animateSettings.offset === 'function' ? animateSettings.offset() : animateSettings.offset), 10)); // Location to scroll to
+			var endLocation = isNum ? anchor : getEndLocation(anchorElem, headerHeight, parseInt((typeof animateSettings.offset === 'function' ? animateSettings.offset(anchor, toggle) : animateSettings.offset), 10), animateSettings.clip); // Location to scroll to
 			var distance = endLocation - startLocation; // distance to travel
 			var documentHeight = getDocumentHeight();
 			var timeLapsed = 0;
@@ -347,19 +429,20 @@
 				var currentLocation = window.pageYOffset;
 
 				// Check if the end location has been reached yet (or we've hit the end of the document)
-				if ( position == endLocation || currentLocation == endLocation || ((startLocation < endLocation && window.innerHeight + currentLocation) >= documentHeight )) {
+				if (position == endLocation || currentLocation == endLocation || ((startLocation < endLocation && window.innerHeight + currentLocation) >= documentHeight)) {
 
 					// Clear the animation timer
-					smoothScroll.cancelScroll();
+					smoothScroll.cancelScroll(true);
 
 					// Bring the anchored element into focus
 					adjustFocus(anchor, endLocation, isNum);
 
-					// Run callback after animation complete
-					animateSettings.after(anchor, toggle);
+					// Emit a custom event
+					emitEvent('scrollStop', animateSettings, anchor, toggle);
 
 					// Reset start
 					start = null;
+					animationInterval = null;
 
 					return true;
 
@@ -377,7 +460,7 @@
 				position = startLocation + (distance * easingPattern(animateSettings, percentage));
 				window.scrollTo(0, Math.floor(position));
 				if (!stopAnimateScroll(position, endLocation)) {
-					window.requestAnimationFrame(loopAnimateScroll);
+					animationInterval = window.requestAnimationFrame(loopAnimateScroll);
 					start = timestamp;
 				}
 			};
@@ -387,36 +470,18 @@
 			 * @link https://github.com/cferdinandi/smooth-scroll/issues/45
 			 */
 			if (window.pageYOffset === 0) {
-				window.scrollTo( 0, 0 );
+				window.scrollTo(0, 0);
 			}
 
-			// Run callback before animation starts
-			animateSettings.before(anchor, toggle);
+			// Update the URL
+			updateURL(anchor, isNum, animateSettings);
+
+			// Emit a custom event
+			emitEvent('scrollStart', animateSettings, anchor, toggle);
 
 			// Start scrolling animation
-			smoothScroll.cancelScroll();
+			smoothScroll.cancelScroll(true);
 			window.requestAnimationFrame(loopAnimateScroll);
-
-
-		};
-
-		/**
-		 * Handle has change event
-		 */
-		var hashChangeHandler = function (event) {
-
-			// Only run if there's an anchor element to scroll to
-			if (!anchor) return;
-
-			// Reset the anchor element's ID
-			anchor.id = anchor.getAttribute('data-scroll-id');
-
-			// Scroll to the anchored content
-			smoothScroll.animateScroll(anchor, toggle);
-
-			// Reset anchor and toggle
-			anchor = null;
-			toggle = null;
 
 		};
 
@@ -431,6 +496,10 @@
 			// Don't run if right-click or command/control + click
 			if (event.button !== 0 || event.metaKey || event.ctrlKey) return;
 
+			// Check if event.target has closest() method
+			// By @totegi - https://github.com/cferdinandi/smooth-scroll/pull/401/
+			if(!('closest' in event.target))return;
+
 			// Check if a smooth scroll link was clicked
 			toggle = event.target.closest(selector);
 			if (!toggle || toggle.tagName.toLowerCase() !== 'a' || event.target.closest(settings.ignore)) return;
@@ -438,53 +507,40 @@
 			// Only run if link is an anchor and points to the current page
 			if (toggle.hostname !== window.location.hostname || toggle.pathname !== window.location.pathname || !/#/.test(toggle.href)) return;
 
-			// Get the sanitized hash
-			var hash;
-			try {
-				hash = escapeCharacters(decodeURIComponent(toggle.hash));
-			} catch(e) {
-				hash = escapeCharacters(toggle.hash);
-			}
-
-			// If the hash is empty, scroll to the top of the page
-			if (hash === '#') {
-
-				// Prevent default link behavior
-				event.preventDefault();
-
-				// Set the anchored element
-				anchor = document.body;
-
-				// Save or create the ID as a data attribute and remove it (prevents scroll jump)
-				var id = anchor.id ? anchor.id : 'smooth-scroll-top';
-				anchor.setAttribute('data-scroll-id', id);
-				anchor.id = '';
-
-				// If no hash change event will happen, fire manually
-				// Otherwise, update the hash
-				if (window.location.hash.substring(1) === id) {
-					hashChangeHandler();
-				} else {
-					window.location.hash = id;
-				}
-
-				return;
-
-			}
+			// Get an escaped version of the hash
+			var hash = escapeCharacters(decode(toggle.hash));
 
 			// Get the anchored element
-			anchor = document.querySelector(hash);
+			var anchor = settings.topOnEmptyHash && hash === '#' ? document.documentElement : document.querySelector(hash);
+			anchor = !anchor && hash === '#top' ? document.documentElement : anchor;
 
-			// If anchored element exists, save the ID as a data attribute and remove it (prevents scroll jump)
+			// If anchored element exists, scroll to it
 			if (!anchor) return;
-			anchor.setAttribute('data-scroll-id', anchor.id);
-			anchor.id = '';
+			event.preventDefault();
+			smoothScroll.animateScroll(anchor, toggle);
 
-			// If no hash change event will happen, fire manually
-			if (toggle.hash === window.location.hash) {
-				event.preventDefault();
-				hashChangeHandler();
-			}
+		};
+
+		/**
+		 * Animate scroll on popstate events
+		 */
+		var popstateHandler = function (event) {
+			// Stop if history.state doesn't exist (ex. if clicking on a broken anchor link).
+			// fixes `Cannot read property 'smoothScroll' of null` error getting thrown.
+			if (history.state === null) return; 
+
+			// Only run if state is a popstate record for this instantiation
+			if (!history.state.smoothScroll || history.state.smoothScroll !== JSON.stringify(settings)) return;
+
+			// Only run if state includes an anchor
+			if (!history.state.anchor) return;
+
+			// Get the anchor
+			var anchor = document.querySelector(escapeCharacters(decode(history.state.anchor)));
+			if (!anchor) return;
+
+			// Animate scroll to anchor link
+			smoothScroll.animateScroll(anchor, null, {updateURL: false});
 
 		};
 
@@ -511,6 +567,7 @@
 			// Remove event listeners
 			document.removeEventListener('click', clickHandler, false);
 			window.removeEventListener('resize', resizeThrottler, false);
+			window.removeEventListener('popstate', popstateHandler, false);
 
 			// Cancel any scrolls-in-progress
 			smoothScroll.cancelScroll();
@@ -523,6 +580,7 @@
 			headerHeight = null;
 			eventTimeout = null;
 			animationInterval = null;
+
 		};
 
 		/**
@@ -532,7 +590,7 @@
 		smoothScroll.init = function (options) {
 
 			// feature test
-			if (!supports) return;
+			if (!supports()) throw 'Smooth Scroll: This browser does not support the required JavaScript methods and browser APIs.';
 
 			// Destroy any existing initializations
 			smoothScroll.destroy();
@@ -545,12 +603,14 @@
 			// When a toggle is clicked, run the click handler
 			document.addEventListener('click', clickHandler, false);
 
-			// Listen for hash changes
-			window.addEventListener('hashchange', hashChangeHandler, false);
-
 			// If window is resized and there's a fixed header, recalculate its size
 			if (fixedHeader) {
 				window.addEventListener('resize', resizeThrottler, false);
+			}
+
+			// If updateURL and popState are enabled, listen for pop events
+			if (settings.updateURL && settings.popstate) {
+				window.addEventListener('popstate', popstateHandler, false);
 			}
 
 		};
